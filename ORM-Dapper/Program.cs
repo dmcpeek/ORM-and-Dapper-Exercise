202909f08631f4893c85d27d408b223dd05f3c47
Doug McPeek <dmcpeek@outlook.com>
2023-05-12 11:27:56 -0600

    Check in

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -20,58 +20,58 @@ namespace ORM_Dapper
             IDbConnection conn = new MySqlConnection(connString);
 
             #region Department   
-            //var departmentRepo = new DapperDepartmentRepository(conn);
-            //// GET Departments
-            //var departments = departmentRepo.GetAllDepartments();
+            var departmentRepo = new DapperDepartmentRepository(conn);
+            // GET Departments
+            var departments = departmentRepo.GetAllDepartments();
 
-            //foreach (var department in departments)
-            //{
-            //    Console.WriteLine(department.DepartmentID);
-            //    Console.WriteLine(department.Name);
-            //    Console.WriteLine();
-            //}
-            //// END GET
-
-            //// INSERT Department
-            //Console.Write("Type a new department name: ");
-            //var newDepartment = Console.ReadLine();
-            //departmentRepo.InsertDepartment(newDepartment);
-            //// END INSERT
-
-            //// GET Departments
-            //departments = departmentRepo.GetAllDepartments();
-
-            //foreach (var department in departments)
-            //{
-            //    Console.WriteLine(department.DepartmentID);
-            //    Console.WriteLine(department.Name);
-            //    Console.WriteLine();
-            //}
-            //// END GET
-            #endregion
-
-            var productRepository = new DapperProductRepository(conn);
-
-            var productToUpdate = productRepository.GetAllProducts(940);
-            
-            productRepository.UpdateProduct(productToUpdate);
-            productToUpdate.Name = "UPDATED";
-            productToUpdate.Price = 150.00;
-            productToUpdate.StockLevel = 1;
-            productToUpdate.CategoryID = 1;
-            productToUpdate.OnSale = true;
-
-            var products = productRepository.GetAllProducts();
-            foreach (var product in products) 
+            foreach (var department in departments)
             {
-                Console.WriteLine(product.ProductID);
-                Console.WriteLine(product.Name);
-                Console.WriteLine(product.Price);
-                Console.WriteLine(product.CategoryID);
-                Console.WriteLine(product.OnSale);
-                Console.WriteLine(product.StockLevel);
+                Console.Write($"{department.DepartmentID} {department.Name}");
                 Console.WriteLine();
             }
+            // END GET
+
+            // INSERT Department
+            Console.Write("Type a new department name: ");
+            var newDepartment = Console.ReadLine();
+            departmentRepo.InsertDepartment(newDepartment);
+            // END INSERT
+
+            // GET Departments
+            departments = departmentRepo.GetAllDepartments();
+
+            foreach (var department in departments)
+            {
+                Console.Write($"{department.DepartmentID} {department.Name}");
+                Console.WriteLine();
+            }
+            // END GET
+            #endregion
+
+            #region Product
+            //var productRepository = new DapperProductRepository(conn);
+
+            //var productToUpdate = productRepository.GetAllProducts(940);
+
+            //productRepository.UpdateProduct(productToUpdate);
+            //productToUpdate.Name = "UPDATED";
+            //productToUpdate.Price = 150.00;
+            //productToUpdate.StockLevel = 1;
+            //productToUpdate.CategoryID = 1;
+            //productToUpdate.OnSale = true;
+
+            //var products = productRepository.GetAllProducts();
+            //foreach (var product in products) 
+            //{
+            //    Console.WriteLine(product.ProductID);
+            //    Console.WriteLine(product.Name);
+            //    Console.WriteLine(product.Price);
+            //    Console.WriteLine(product.CategoryID);
+            //    Console.WriteLine(product.OnSale);
+            //    Console.WriteLine(product.StockLevel);
+            //    Console.WriteLine();
+            //}
+            #endregion
 
         }
     }
